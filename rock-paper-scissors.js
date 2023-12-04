bb881005fc919910b3b0424bd7e032c202df16a5
JosephPhan <146134348+joseph-phan@users.noreply.github.com>
2023-12-04 13:27:05 -0800

    Remove playRound fucntion and rewrite entire function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,10 @@
 function getComputerChoice() {
     const computerMove = ['rock', 'paper', 'scissors'];
     const random = Math.floor(Math.random() * computerMove.length);
-    return (random, computerMove[random]);
+    return (computerMove[random]);
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection) {
-        return result = 'You win!';
-    } else if (playerSelection == computerSelection) {
-        return result = 'It\'s a tie! Try again!';
-    } else {
-        return result = 'You lose!';
-    }
+    
+    
 }
