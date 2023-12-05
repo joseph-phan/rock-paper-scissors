34c11e2e20d9533ec390ab2a6fb95fcf469475bd
JosephPhan <146134348+joseph-phan@users.noreply.github.com>
2023-12-05 11:35:27 -0800

    Made the if statements shorter

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,24 +5,19 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return result = 'You win!';
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return result = 'You lose!';
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return result = 'You lose!';
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return result = 'You win!';
+    if (playerSelection === computerSelection) {
+        return result = 'It is a tie! Try again!';
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return result = 'Congrats! You win!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return result = 'You win!';
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return result = 'You lose!';
+        return result = 'Congrats! You win!';
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return result = 'Congrats! You win!';
     } else {
-        return result = 'It\'s a tie!';
+        return result = 'You lose!';
     }
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
-console.log(getComputerChoice());
