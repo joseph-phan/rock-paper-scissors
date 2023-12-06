dd292cb629892d8aabdd6c8bb960d14b07a1d044
JosephPhan <146134348+joseph-phan@users.noreply.github.com>
2023-12-06 11:55:22 -0800

    Add while loop to repeat if the round is a tie

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,9 +5,13 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return result = 'It is a tie! Try again!';
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    while(true) {
+        if (playerSelection === computerSelection) break; {
+            return result = 'It is a tie! Try again!';
+        }
+    }
+
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
         return result = 'Congrats! You win!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         return result = 'Congrats! You win!';
